753186e4019b6c919e10248747b06f7faf4d6343
lje64257@gmail.com <lje64257@gmail.com>
2026-06-15 16:27:27 +0900

    CatEscape 등록

---
+++ CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject hpGauge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpGauge = GameObject.Find("hpGauge");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DecreaseHp() {
+        hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+}
